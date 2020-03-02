a76feeb4bc72a6d5109b22df7cfc41c872b75ddf
nleinone <nikoleino91@gmail.com>
2020-03-02 11:33:27 +0200

    Added colours to current score. Added score updater method

--- DeedsBeeActivityChallenge/app/src/main/java/com/example/deedsbeeactivitychallenge/MainActivity.java
+++ DeedsBeeActivityChallenge/app/src/main/java/com/example/deedsbeeactivitychallenge/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.deedsbeeactivitychallenge;
 
 import android.content.BroadcastReceiver;
-import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,9 +10,6 @@ import com.example.deedsbeeactivitychallenge.ui.main.BackgroundDetectedActivitie
 import com.example.deedsbeeactivitychallenge.ui.main.Constants;
 import com.example.deedsbeeactivitychallenge.ui.main.Fragment1;
 import com.example.deedsbeeactivitychallenge.ui.main.Fragment2;
-import com.google.android.gms.location.ActivityRecognitionClient;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -19,51 +17,20 @@ import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
-import com.google.android.gms.location.ActivityRecognitionClient;
 import com.google.android.gms.location.DetectedActivity;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import android.content.SharedPreferences;
-import android.app.PendingIntent;
-import android.preference.PreferenceManager;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.deedsbeeactivitychallenge.ui.main.SectionsPagerAdapter;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity implements Fragment1.OnFragmentInteractionListener,
         Fragment2.OnFragmentInteractionListener {
 
-
-    //Activity recognition:
-    private Context mContext;
-    public static final String DETECTED_ACTIVITY = ".DETECTED_ACTIVITY";
     BroadcastReceiver broadcastReceiver;
     private TextView txtActivity, txtConfidence;
-    //Define an ActivityRecognitionClient//
-
-    SharedPreferences.OnSharedPreferenceChangeListener prefListener =
-            new SharedPreferences.OnSharedPreferenceChangeListener() {
-                public void onSharedPreferenceChanged(SharedPreferences prefs,
-                                                      String key) {
-                    if (key.equals(DETECTED_ACTIVITY)) {
-                    }
-                }
-            };
-
-    private ActivityRecognitionClient mActivityRecognitionClient;
 
     private int[] icons = {
             R.drawable.star,
@@ -81,8 +48,6 @@ public class MainActivity extends AppCompatActivity implements Fragment1.OnFragm
         //Activity recognition:
         txtActivity = findViewById(R.id.activityTv);
         txtConfidence = findViewById(R.id.confTv);
-
-        mContext = this;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -164,22 +129,22 @@ public class MainActivity extends AppCompatActivity implements Fragment1.OnFragm
             }
         }
 
-        Log.e("MainActivity", "User activity: " + label + ", Confidence: " + confidence);
-
         txtActivity = findViewById(R.id.activityTv);
         txtConfidence = findViewById(R.id.confTv);
 
         if (confidence > Constants.CONFIDENCE) {
             try
             {
-                Log.e("MainActivity", "User activity: " + label + ", Confidence: " + confidence);
+                Log.v("MainActivity", "User activity: " + label + ", Confidence: " + confidence);
                 txtActivity.setText(label);
                 String text = "Confidence: " + confidence;
+                updateScore(label);
                 txtConfidence.setText(text);
             }
             catch(Exception e)
             {
-                Log.e("MainActivity", "User activity: " + label + ", Confidence: " + confidence);
+                Log.v("MainActivity e: ", e.toString());
+                Log.v("MainActivity ERROR", "User activity: " + label + ", Confidence: " + confidence);
             }
         }
     }
@@ -188,6 +153,160 @@ public class MainActivity extends AppCompatActivity implements Fragment1.OnFragm
         Log.v("MainActivity", "startTracking");
         Intent intent1 = new Intent(MainActivity.this, BackgroundDetectedActivitiesService.class);
         startService(intent1);
+    }
+
+    private void edit_score(int current_score_int, TextView scoreTv, Boolean isPenalty, int pointModifier)
+    {
+
+        if(isPenalty)
+        {
+            current_score_int = current_score_int - pointModifier;
+            String current_score_string = Integer.toString(current_score_int);
+            scoreTv.setText(current_score_string);
+            updateScoreColor(scoreTv);
+        }
+        else
+        {
+            current_score_int = current_score_int + pointModifier;
+            String current_score_string = Integer.toString(current_score_int);
+            scoreTv.setText(current_score_string);
+            updateScoreColor(scoreTv);
+        }
+    }
+
+    private void updateScoreColor(TextView scoreTv)
+    {
+        String current_score_string = scoreTv.getText().toString();
+        Log.v("MainActivity", "current_score_string 2: " + current_score_string);
+        int current_score_int = Integer.parseInt(current_score_string);
+
+        Log.v("MainActivity", "current_score_int 2: " + current_score_int);
+
+        if(current_score_int < 100)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color1));
+        }
+        else if(current_score_int < 200)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color2));
+        }
+        else if(current_score_int < 300)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color3));
+        }
+        else if(current_score_int < 400)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color4));
+        }
+        else if(current_score_int < 500)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color5));
+        }
+        else if(current_score_int < 600)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color6));
+        }
+        else if(current_score_int < 700)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color7));
+        }
+        else if(current_score_int < 800)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color8));
+        }
+        else if(current_score_int < 900)
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color9));
+        }
+        else
+        {
+            scoreTv.setTextColor(getResources().getColor(R.color.color10));
+        }
+    }
+
+    private void updateScore(String activity_status)
+    {
+
+        TextView scoreTv = findViewById(R.id.scoreTextView);
+        String current_score_string = scoreTv.getText().toString();
+        int current_score_int = Integer.parseInt(current_score_string);
+
+        int still_penalty = Constants.PENALTY_SCORE;
+        int walking_score = Constants.WALKING_SCORE;
+        int running_score = Constants.RUNNING_SCORE;
+
+        Log.v("MainActivity", "activity_status: " + activity_status);
+
+        //User will lose points when still.
+        if(activity_status.equals(getString(R.string.still)))
+        {
+            Boolean isPenalty = true;
+            Log.v("MainActivity", "current_score_int: " + current_score_int);
+            if(current_score_int > 0)
+            {
+                Log.v("MainActivity", "current_score_int: " + current_score_string);
+                edit_score(current_score_int, scoreTv, isPenalty, still_penalty);
+            }
+            else
+            {
+                updateScoreColor(scoreTv);
+            }
+        }
+
+        //User will gain points when walking.
+        else if(activity_status.equals(getString(R.string.walking)))
+        {
+            if (current_score_int < Constants.MAX_SCORE)
+            {
+                Boolean isPenalty = false;
+                edit_score(current_score_int, scoreTv, isPenalty, walking_score);
+            }
+
+        }
+
+        //User will lose points when on foot.
+        else if(activity_status.equals(getString(R.string.foot)))
+        {
+            if(current_score_int < Constants.MAX_SCORE)
+            {
+                Boolean isPenalty = false;
+                edit_score(current_score_int, scoreTv, isPenalty, walking_score);
+            }
+        }
+
+        //User will lose points when running.
+        else if(activity_status.equals(getString(R.string.running)))
+        {
+            if(current_score_int < Constants.MAX_SCORE)
+            {
+                Boolean isPenalty = false;
+                edit_score(current_score_int, scoreTv, isPenalty, running_score);
+            }
+        }
+
+        //User will lose points when cycling.
+        else if(activity_status.equals(getString(R.string.bicycle)))
+        {
+            if(current_score_int < Constants.MAX_SCORE)
+            {
+                Boolean isPenalty = false;
+                edit_score(current_score_int, scoreTv, isPenalty, running_score);
+            }
+
+        }
+
+        //User will lose points when in vehicle
+        else
+        {
+            if(current_score_int > Constants.MIN_SCORE) {
+
+                Boolean isPenalty = true;
+                edit_score(current_score_int, scoreTv, isPenalty, still_penalty);
+            }
+        }
+
+        //Change the color of the score textView:
+
     }
 
 }
--- DeedsBeeActivityChallenge/app/src/main/java/com/example/deedsbeeactivitychallenge/ui/main/Constants.java
+++ DeedsBeeActivityChallenge/app/src/main/java/com/example/deedsbeeactivitychallenge/ui/main/Constants.java
@@ -9,4 +9,14 @@ public class Constants {
     static final long DETECTION_INTERVAL_IN_MILLISECONDS = 10 * 1000;
 
     public static final int CONFIDENCE = 70;
+
+    public static final int WALKING_SCORE = 10;
+
+    public static final int RUNNING_SCORE = 25;
+
+    public static final int PENALTY_SCORE = 1;
+
+    public static final int MAX_SCORE = 1000;
+
+    public static final int MIN_SCORE = 0;
 }
